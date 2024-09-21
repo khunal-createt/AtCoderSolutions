b34f3e4edfc413051e0038dd9128f6c54d3fba0d
khunal-createt <khunal.shaik@gmail.com>
2024-09-21 21:43:51 +0530

    371 Jiro
    solution of AtCoder beginner contest 371 1st problem solution Jiro.

---
+++ Beginner-Contest/src/test/java/com/atCoder/bedinnerContest/BeginnerContestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atCoder.bedinnerContest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginnerContestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
